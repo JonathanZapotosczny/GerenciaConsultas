a9af8565a48f9d1f8274416d8793f574f29d6668
Jonathan Zapotosczny <JonathanGM.HD@gmail.com>
2023-02-16 16:25:27 -0300

    Configurações Iniciais...

---
+++ gerenciaconsultas/src/test/java/br/com/gerenciaconsultas/GerenciaconsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciaconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaconsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
